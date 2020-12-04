6de8343636043ec5ac7b305e12e3270ef7236433
yar.mor <ta.tak.hopm@gmail.com>
2020-12-04 16:22:50 +0200

    implemented all methods

--- src/main/java/ua/edu/ucu/autocomplete/PrefixMatches.java
+++ src/main/java/ua/edu/ucu/autocomplete/PrefixMatches.java
@@ -1,40 +1,88 @@
 package ua.edu.ucu.autocomplete;
 
 import ua.edu.ucu.tries.Trie;
+import ua.edu.ucu.tries.Tuple;
+import ua.edu.ucu.collections.StringKeeper;
 
-/**
- *
- * @author andrii
- */
+import java.util.ArrayList;
+
+
 public class PrefixMatches {
 
-    private Trie trie;
+    private final Trie trie;
 
     public PrefixMatches(Trie trie) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.trie = trie;
     }
 
     public int load(String... strings) {
-        throw new UnsupportedOperationException("Not supported yet.");        
+        String[] words;
+        for(String str: strings) {
+            words = str.split(" ");
+
+            for (String word: words) {
+                if (word.length() > 2) {
+                    // convert the word to lowercase for more proper autocomplete work
+                    // source: https://www.cs.princeton.edu/courses/archive/fall13/cos226/assignments/autocomplete.html
+                    this.trie.add(new Tuple(word.toLowerCase(), word.length()));
+                }
+            }
+
+        }
+        return size();
     }
 
     public boolean contains(String word) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (word == null || word.equals("")) {
+            throw new IllegalArgumentException("Empty strings are not maintained.");
+        }
+        return this.trie.contains(word.toLowerCase());
     }
 
     public boolean delete(String word) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (word == null || word.equals("")) {
+            throw new IllegalArgumentException("Empty strings are not maintained.");
+        }
+        return this.trie.delete(word.toLowerCase());
     }
 
     public Iterable<String> wordsWithPrefix(String pref) {
-        throw new UnsupportedOperationException("Not supported yet.");        
+        if (pref == null || pref.length() < 2) {
+            throw new IllegalArgumentException("Prefix should have >= 2 characters");
+        }
+
+        int theLongestWordEver = 189819;
+        return wordsWithPrefix(pref, theLongestWordEver);
     }
 
     public Iterable<String> wordsWithPrefix(String pref, int k) {
-        throw new UnsupportedOperationException("Not supported yet.");        
+        if (pref == null || pref.length() < 2) {
+            throw new IllegalArgumentException("Prefix should have >= 2 characters");
+        }
+        if (k <= 0) {
+            throw new IllegalArgumentException("Indicator k has to be positive valued integer");
+        }
+
+        int minPrefLen = pref.length();
+        if (pref.length() == 2) {
+            minPrefLen = 3;
+        }
+
+        StringKeeper allWordsWithPrefix = (StringKeeper) this.trie.wordsWithPrefix(pref);
+        ArrayList<String> wordsThatMatch = new ArrayList<>();
+
+        int wordLength;
+        for (String word: allWordsWithPrefix.toArray()) {
+            wordLength = word.length();
+            if (minPrefLen <= wordLength && wordLength <= minPrefLen + k - 1) {
+                wordsThatMatch.add(word);
+            }
+        }
+
+        return wordsThatMatch;
     }
 
     public int size() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return this.trie.size();
     }
 }
--- src/main/java/ua/edu/ucu/tries/RWayTrie.java
+++ src/main/java/ua/edu/ucu/tries/RWayTrie.java
@@ -1,35 +1,207 @@
 package ua.edu.ucu.tries;
 
+
+import ua.edu.ucu.collections.Queue;
+import ua.edu.ucu.collections.StringKeeper;
+
+
 public class RWayTrie implements Trie {
+    private static final int R = 26;
+    private static final char FIRST_ALPH_CH = 'a';
+    private static final char LAST_ALPH_CH = 'z';
+
+    private Node root = new Node();
+
+    private static class Node {
+        private Object val;
+        private final Node[] next = new Node[R];
+    }
+
+    /*
+    * Throw IllegalArgumentException if character is not a letter.
+    */
+    private void ValidateCharacter(char c) {
+        if (c < FIRST_ALPH_CH || c > LAST_ALPH_CH) {
+            throw new IllegalArgumentException("Not valid character! ");
+        }
+    }
 
     @Override
     public void add(Tuple t) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.root = add(this.root, t.term, t.weight, 0);
+    }
+
+    /*
+    * Add a new node to the trie if there is no such a node.
+    * Change value associated with key if in subtrie rooted at node.
+    *
+    * @source: «Robert Sedgewick, Kevin Wayne Algorithms, 4th Edition Addison»
+    *
+    * @param: (Node) node - node to insert
+    * @param: (String) key - a word to insert
+    * @param: (int) value - weight of a word
+    * @param: (int) dest - index of a node of where to insert
+    *
+    * @return: Node
+    */
+    private Node add(Node node, String key, int value, int dest) {
+        Node newNode = node;
+        if (node == null)
+            newNode = new Node();
+
+        if (dest == key.length()) {
+            newNode.val = value;
+            return newNode;
+        }
+
+        // Use dth key char to identify subtrie.
+        char c = key.charAt(dest);
+        ValidateCharacter(c);
+
+        // We subtract FIRST_ALPH_CH from c as our array has bounds [0, 25], while
+        // ASCII characters start with 65
+        newNode.next[c - FIRST_ALPH_CH] = add(newNode.next[c - FIRST_ALPH_CH], key, value, dest + 1);
+        return newNode;
     }
 
     @Override
     public boolean contains(String word) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Node node = get(root, word, 0);
+        return node != null;
+    }
+
+    /*
+     * Return True if such word exists in a trie.
+     *
+     * @source: «Robert Sedgewick, Kevin Wayne Algorithms, 4th Edition Addison»
+     *
+     * @param: (String) key - a word to delete
+     * @return: boolean
+     */
+    private Node get(Node node, String key, int dest) {
+        if (node == null) {
+            return null;
+        }
+
+        if (dest == key.length()) {
+            return node;
+        }
+
+        // Use dth key char to identify subtrie.
+        char c = key.charAt(dest);
+        return get(node.next[c - FIRST_ALPH_CH], key, dest + 1);
     }
 
     @Override
     public boolean delete(String word) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (contains(word)) {
+            root = delete(root, word, 0);
+            return true;
+        }
+        return false;
+    }
+
+    /*
+     * Return a node of the trie if such exists and delete its connection.
+     *
+     * @source: «Robert Sedgewick, Kevin Wayne Algorithms, 4th Edition Addison»
+     *
+     * @param: (Node) node - node to start with
+     * @param: (String) key - a word to delete
+     * @param: (int) dest - index of a node of where to start
+     *
+     * @return: Node
+     */
+    private Node delete(Node node, String key, int dest) {
+        if (node == null) {
+            return null;
+        }
+
+        if (dest == key.length()) {
+            node.val = null;
+        } else {
+            char c = key.charAt(dest);
+            ValidateCharacter(c);
+
+            node.next[c - FIRST_ALPH_CH] = delete(node.next[c - FIRST_ALPH_CH], key, dest + 1);
+        }
+
+        if (node.val != null) {
+            return node;
+        }
+
+        for (char c = 0; c < R; c++) {
+            if (node.next[c] != null) {
+                return node;
+            }
+        }
+        return null;
     }
 
     @Override
     public Iterable<String> words() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return wordsWithPrefix("");
     }
 
     @Override
     public Iterable<String> wordsWithPrefix(String s) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Queue q = new Queue();
+        collect(get(root, s, 0), s, q);
+
+        int queueSize = q.size();
+        StringKeeper sk = new StringKeeper(queueSize);
+
+        for (int i = 0; i < queueSize; i++) {
+            sk.add((String) q.dequeue(), i);
+        }
+        return sk;
+    }
+
+    /*
+     * Add a word to a queue.
+     *
+     * @source: «Robert Sedgewick, Kevin Wayne Algorithms, 4th Edition Addison»
+     *
+     * @param: (Node) node - node to start with
+     * @param: (String) pre - a word to collect
+     * @param: (Queue) q - collection to store elements
+     *
+     * @return: Null
+     */
+    private void collect(Node node, String pre, Queue q) {
+        if (node == null) {
+            return;
+        }
+
+        if (node.val != null) {
+            q.enqueue(pre);
+        }
+
+        for (char c = FIRST_ALPH_CH; c < LAST_ALPH_CH; c++) {
+            collect(node.next[c - FIRST_ALPH_CH], pre + c, q);
+        }
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        return size(this.root);
+    }
+
+    private int size(Node node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int cnt = 0;
+        if (node.val != null)  {
+            cnt++;
+        }
+
+        for (char c = 0; c < R; c++) {
+            cnt += size(node.next[c]);
+        }
+
+        return cnt;
     }
 
 }
