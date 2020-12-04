f50e4be12c2ceeb2c1443f7e7eac0da651d6f862
yar.mor <ta.tak.hopm@gmail.com>
2020-12-04 10:25:55 +0200

    Autocomplete initial commit

---
+++ src/main/java/ua/edu/ucu/autocomplete/PrefixMatches.java
@@ -0,0 +1,40 @@
+package ua.edu.ucu.autocomplete;
+
+import ua.edu.ucu.tries.Trie;
+
+/**
+ *
+ * @author andrii
+ */
+public class PrefixMatches {
+
+    private Trie trie;
+
+    public PrefixMatches(Trie trie) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int load(String... strings) {
+        throw new UnsupportedOperationException("Not supported yet.");        
+    }
+
+    public boolean contains(String word) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean delete(String word) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Iterable<String> wordsWithPrefix(String pref) {
+        throw new UnsupportedOperationException("Not supported yet.");        
+    }
+
+    public Iterable<String> wordsWithPrefix(String pref, int k) {
+        throw new UnsupportedOperationException("Not supported yet.");        
+    }
+
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
---
+++ src/main/java/ua/edu/ucu/tries/RWayTrie.java
@@ -0,0 +1,35 @@
+package ua.edu.ucu.tries;
+
+public class RWayTrie implements Trie {
+
+    @Override
+    public void add(Tuple t) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean contains(String word) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean delete(String word) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Iterable<String> words() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Iterable<String> wordsWithPrefix(String s) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+}
